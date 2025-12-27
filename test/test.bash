6b9438dce61c6bf37663edc72f9ddb085968677a
TaikiAkiyama <s24c1005hq@s.chibakoudai.jp>
2025-12-27 23:28:55 +0900

    テスト項目を追加

--- test/test.bash
+++ test/test.bash
@@ -12,7 +12,7 @@ source ~/.bashrc
 export PYTHONUNBUFFERED=1
 
 rm -rf /dev/shm/fastrtps_*
-
+pkill -f register
 LOGfile=/tmp/register.log
 rm -f $LOGfile
 
@@ -22,14 +22,23 @@ sleep 3
 
 (sleep 2; echo "1") | ros2 run register scanner 
 sleep 2
-
 cat $LOGfile | grep 'Apple 150' || { echo "Test Failed: Apple not found"; kill $PID; exit 1; }
 
 
 (sleep 2; echo "0") | ros2 run register scanner
 sleep 2
-
 cat $LOGfile | grep 'Error: ID' || { echo "Test Failed: Error message not found"; kill $PID; exit 1; }
 
+(sleep 2; echo "a") | ros2 run register scanner
+sleep 2
+cat $LOGfile | grep 'Input Error' || { echo "Test Failed: Error message not found"; kill $PID; exit 1; }
+
+(sleep 2; echo "あ") | ros2 run register scanner
+sleep 2
+cat $LOGfile | grep 'Input Error' || { echo "Test Failed: Error message not found"; kill $PID; exit 1; }
+
+(sleep 2; echo "") | ros2 run register scanner
+sleep 2
+cat $LOGfile | grep 'Input Error' || { echo "Test Failed: Error message not found"; kill $PID; exit 1; }
 echo "All Tests Passed"
 kill $PID
