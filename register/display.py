0c2e7e0700ee759888f4ae558c1424fe259bd1ae
TaikiAkiyama <s24c1005hq@s.chibakoudai.jp>
2025-12-27 23:30:29 +0900

    コードを編集

--- register/display.py
+++ register/display.py
@@ -3,14 +3,14 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int32
+from std_msgs.msg import String 
 import sys
 
 class PriceDisplay(Node):
     def __init__(self):
         super().__init__('price_display')
         self.subscription = self.create_subscription(
-            Int32,
+            String,
             'scanned_id',
             self.listener_callback,
             10)
@@ -21,12 +21,18 @@ class PriceDisplay(Node):
         }
 
     def listener_callback(self, msg):
-        item_id = msg.data
-        if item_id in self.products:
-            item = self.products[item_id]
-            print(f'{item["name"]} {item["price"]}', flush=True)
-        else:
-            print(f'Error: ID {item_id} not registerred', file=sys.stderr, flush=True) 
+        raw_data = msg.data
+        try:
+            item_id = int(raw_data)
+
+            if item_id in self.products:
+                item = self.products[item_id]
+                print(f'{item["name"]} {item["price"]}', flush=True)
+            else:
+                print(f'Error: ID {item_id} not registered', file=sys.stderr, flush=True) 
+
+        except ValueError:
+             print(f'Input Error: {raw_data} not a number', file=sys.stderr, flush=True)
 
 def main(args=None):
     rclpy.init(args=args)
--- register/scanner.py
+++ register/scanner.py
@@ -3,33 +3,28 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int32
+from std_msgs.msg import String 
 import sys
 
 class BarcodeScanner(Node):
     def __init__(self):
         super().__init__('barcode_scanner')
-        self.publisher_ = self.create_publisher(Int32, 'scanned_id', 10)
+        self.publisher_ = self.create_publisher(String, 'scanned_id', 10)
 
     def scan_loop(self):
         try:
             while rclpy.ok():
                 line = sys.stdin.readline()
-            
                 if not line:
                     break
-
                 line = line.strip()
-
                 if not line:
                     continue
 
-                try:
-                    msg = Int32()
-                    msg.data = int(line)
-                    self.publisher_.publish(msg)
-                except ValueError:
-                    pass
+                
+                msg = String()
+                msg.data = line
+                self.publisher_.publish(msg)
 
         except KeyboardInterrupt:
             pass
@@ -37,9 +32,7 @@ class BarcodeScanner(Node):
 def main(args=None):
     rclpy.init(args=args)
     node = BarcodeScanner()
-    
     node.scan_loop()
-
     node.destroy_node()
     rclpy.shutdown()
 
